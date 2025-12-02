0c00b5f9be7a20781dc4c902935c2143ea9d46b3
hayford <hayford@edspike.com>
2025-12-02 19:31:13 +0000

    updates

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,1 @@
 export { default as CheckInOutSidebar } from './layout/CheckInOutSidebar.vue'
-export { default as GlobalTextField } from './shared/GlobalTextField.vue'
-export { default as GlobalButton } from './shared/GlobalButton.vue'
